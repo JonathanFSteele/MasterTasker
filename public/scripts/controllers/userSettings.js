452734c037f382f3be996af7152933224525dc37
JonathanLoya <jloya@cs.nmsu.edu>
2017-04-01 15:07:26 -0600

    visual buttons and functions are now shown in userSettings.html

--- public/scripts/controllers/userSettings.js
+++ public/scripts/controllers/userSettings.js
@@ -1,6 +1,6 @@
 app.controller('userSettingsController', function($scope) {
   // create a message to display in our view
-  $scope.message = 'This is the User Settings Controller';
+  $scope.message = 'Change your display name, profile image, or password here';
   $scope.userEmail = 'email@email';
   $scope.displayName = 'my name';
 });
