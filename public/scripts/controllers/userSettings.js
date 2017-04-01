31f4969703cb6c58873beef9be233cde7b1a6de5
JonathanLoya <jloya@cs.nmsu.edu>
2017-04-01 14:11:09 -0600

    update to userpage, now using controllers

--- public/scripts/controllers/userSettings.js
+++ public/scripts/controllers/userSettings.js
@@ -1,4 +1,6 @@
 app.controller('userSettingsController', function($scope) {
   // create a message to display in our view
   $scope.message = 'This is the User Settings Controller';
+  $scope.userEmail = 'email@email';
+  $scope.displayName = 'my name';
 });
