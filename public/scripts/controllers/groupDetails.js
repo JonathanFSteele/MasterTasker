ac31d0c6751cd03330eb37be0445ac8edffda71c
Zachary Morgan <zacp635@nmsu.edu>
2017-04-08 14:05:19 -0600

    Made groupDetails page show the groups details! (aside from the tags)

--- public/scripts/controllers/groupDetails.js
+++ public/scripts/controllers/groupDetails.js
@@ -5,8 +5,16 @@ app.controller('groupDetailsController', function($scope, $http, $location) {
   $http.get("/api/GroupDetails/ByID?id="+$location.search().id)
   .then(function(response) {
     console.log("GroupDetails response: ",response);
-    $scope.Tasks = response.data;
+    $scope.DisplayName = response.data[0].DisplayName;
+    $scope.Description = response.data[0].Description;
   });
+
+  $http.get("/api/GroupDetails/GroupMembers?id="+$location.search().id)
+  .then(function(response) {
+    console.log("GroupMembers response: ",response);
+    $scope.GroupMembers = response.data;
+  });
+
 
   // $scope.Groups = [];
 
--- server/groupDetails.js
+++ server/groupDetails.js
@@ -22,6 +22,24 @@ router.db_getGroupByID = function(GroupID)
   return deferred.promise;
 }; //end db_getGroupsList()
 
+router.db_getGroupMembersByID = function(GroupID)
+{
+  var deferred = q.defer(); // Use Q
+  var connection = mysql.createConnection(router.dbConfig);
+  var query_str = "SELECT * FROM tldb.GroupMembers_vw WHERE GroupID=?";
+  var query_var = [GroupID];
+  //var query_var; //null
+  var query = connection.query(query_str, query_var, function (err, rows, fields) {
+      if (err) {
+        deferred.reject(err);
+      }
+      else {
+        deferred.resolve(rows);
+      }
+  });
+  return deferred.promise;
+}; //end db_getGroupsList()
+
 
 //Routes for this module:
 router.use(function timeLog (req, res, next) {
@@ -31,7 +49,7 @@ router.use(function timeLog (req, res, next) {
 
 // define the users default route
 router.get('/', function (req, res) {
-  res.send('Group')
+  res.send('GroupMembers')
 })
 
 // define the list route
@@ -50,6 +68,24 @@ router.get('/ByID', function (req, res) {
      res.status(500).send(error);
    });
 })
+// define the list route
+router.get('/GroupMembers', function (req, res) {
+  var result = [];
+  console.log("req Group members req.query.id: ",req.query.id); //req.authentication will tell you what user is currently logged in (req.authentication.Email - to get the current email for the logged in user.)
+
+  var GroupID = req.query.id;
+
+  router.db_getGroupMembersByID(GroupID)
+   .then(function(rows){
+     console.log('rows result',rows);
+     res.send(rows);
+    },function(error){
+     console.log(error);
+     res.status(500).send(error);
+   });
+})
+
+
 
 
 module.exports = router
