dbed56aee1ef68b6d84a37a4eb6ee6e7e93daaa8
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-04-26 17:25:16 -0600

    Worked on some more bugs with Signup and Login, Got them fixed and cleaned up.

--- public/scripts/controllers/signUp.js
+++ public/scripts/controllers/signUp.js
@@ -1,12 +1,42 @@
+/**
+ * @name signUp.js
+ * @author Jonathan F. Steele - Created: 2/22/17 | LastModified: 4/26/17 - JFS
+ * @summary This is the Controller for creating a New user.
+ * ---------------------------------------------------------------------------
+ * @module app.controller('signUpController')
+ * @function SignUp(DisplayName, Email, Password)
+ *  | @function $http.post("/api/SignUp/", data)
+ *    | @function .then(function(data, response))
+ * @function cancel() -- Sends user back to login Page
+ * ---------------------------------------------------------------------------
+ * @description This is the Controller for creating a New user. Allows the user to input an Email, Password, and
+ * DisplayName. Will send the user back to the Login Page to login with new created account after filling out
+ * all the inputs.
+ **/
 app.controller('signUpController', function($scope, $http, localStorageService, $location, $rootScope) {
-  // create a message to display in our view
   console.log("signUpController Running");
-  $scope.MessageType = 0;
-  $scope.signUp = function(DisplayName, Email, Password){
-    console.log("signUp function called", DisplayName, Email, Password);
+
+  //Scope Variables
+  $scope.LoginClicked = false;
+
+  //signUp Function
+  $scope.signUp = function(DisplayName, Email, Password, ConfirmPassword){
+    console.log("signUp function called", DisplayName, Email, Password, ConfirmPassword);
+    $scope.LoginClicked = true;
+    if(DisplayName == null || Email == null || Password == null || ConfirmPassword == null)
+    {
+      console.log("signUpController: Nothing was filled in so stay on the Page");
+      return;
+    }
+    if(ConfirmPassword != Password)
+    {
+      console.log("signUpController: Passwords do not match!");
+      return;
+    }
     var data = {"DisplayName": DisplayName, "Email": Email, "Password": Password};
     console.log("data: ", data);
     var message="";
+    //Create a New User Post
     $http.post("/api/SignUp/", data)
     .then(function(data, response) {
       console.log("data: ", data);
@@ -26,6 +56,7 @@ app.controller('signUpController', function($scope, $http, localStorageService, 
     });
   };
 
+  //cancel Function
   $scope.cancel = function(){
     $location.path( "/login" );
   }
