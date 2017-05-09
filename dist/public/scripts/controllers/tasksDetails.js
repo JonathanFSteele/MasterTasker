659bc0917ef05ac2fadefb473fdb34dfbf5cfc1d
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-05-09 10:05:05 -0600

    Removed dist folder and dist.zip

--- dist/public/scripts/controllers/tasksDetails.js
+++
@@ -1,5 +0,0 @@
-app.controller('taskDetailsController', function($scope, $location) {
-  // create a message to display in our view
-  $scope.message = 'This is the taskDetails Controller';
-  console.log($location.search().id);
-});
--- dist/public/scripts/controllers/userSettings.js
+++
@@ -1,6 +0,0 @@
-app.controller('userSettingsController', function($scope) {
-  // create a message to display in our view
-  $scope.message = 'Change your display name, profile image, or password here';
-  $scope.userEmail = 'email@email';
-  $scope.displayName = 'my name';
-});
